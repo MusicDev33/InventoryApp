64088026257f44d6133b8d003078b2ef7df15369
Shelby McCowan <smccowan33@gmail.com>
2019-06-18 21:40:58 -0600

    Initial Commit

---
+++ KR Inventory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KR Inventory
+//
+//  Created by Shelby McCowan on 6/18/19.
+//  Copyright © 2019 Shelby McCowan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
